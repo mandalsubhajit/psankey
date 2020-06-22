4d7ea1f17da7d8e7231d13972cf8f5efdd26c3ce
mandalsubhajit <mandalsubhajit@gmail.com>
2020-06-22 09:06:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="psankey",
-    version="0.1.2",
+    version="0.1.3",
     author="Subhajit Mandal",
     author_email="mandalsubhajit@gmail.com",
     description="Package for plotting Sankey diagrams with Python",
